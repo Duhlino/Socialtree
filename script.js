5923fb6542ff8f0eda90ccc11da5d8f331decc7c
Luiz.Eduardo <contatoluizlino@hotmail.com>
2024-06-07 21:33:42 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function ToggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    // Se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // Se estiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    
+  }
+}
